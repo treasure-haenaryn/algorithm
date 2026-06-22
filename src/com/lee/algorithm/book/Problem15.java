712346cc8b7c86461986e25dbd95afbf360edd80
treasure-haenaryn <wotjr7386@naver.com>
2026-06-22 23:10:16 +0900

    큐

--- src/com/lee/algorithm/book/Problem15.java
+++ src/com/lee/algorithm/book/Problem15.java
@@ -6,17 +6,18 @@ import java.util.stream.IntStream;
 
 public class Problem15 {
     public static void main(String[] args) {
-
+        Problem15 p = new Problem15();
+        System.out.println(p.solution(5, 2));
     }
     public int solution(int N, int K) {
         ArrayDeque<Integer> queue = new ArrayDeque<>();
-        IntStream.rangeClosed(1, N).forEach(queue::addLast);
+        IntStream.rangeClosed(1, N).forEach(queue::offer);
 
         while (queue.size() > 1) {
             for (int i = 0; i < K-1; i++) {
-                queue.addFirst(queue.pollFirst());
+                queue.offer(queue.poll());
             }
-            queue.pollFirst();
+            queue.poll();
         }
 
         return queue.pollFirst();
